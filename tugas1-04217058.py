1451f9509482895aa2f97714286e8a1e0f821968
yogatamayoga <31976486+yogatamayoga@users.noreply.github.com>
2017-09-22 10:25:05 +0700

    tugas1-04217058
    please give me grade A ^_^

---
+++ tugas1-04217058.py
@@ -0,0 +1,9 @@
+nama = "Eka Firmansyah Yogatama"
+nim = "04217058"
+umur = 25
+print ("namaku:" + nama)
+print ("nim:" + nim)
+print ("umur")
+print (umur)
+print ("umur 5 tahun ke depan : ")
+print (umur +5)
